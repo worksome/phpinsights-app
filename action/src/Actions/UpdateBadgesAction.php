feec0ef879cb7c82ef215ce2a0a0d02a618b80fd
Oliver Nybroe <olivernybroe@gmail.com>
2020-03-06 11:53:57 +0100

    feat(config): Get min requirements from config file

--- action/src/Actions/UpdateBadgesAction.php
+++ action/src/Actions/UpdateBadgesAction.php
@@ -24,7 +24,7 @@ class UpdateBadgesAction implements Action
      * @param GitHubContext $context
      * @param Configuration $configuration
      */
-    public function __construct( GitHubContext $context, Configuration $configuration)
+    public function __construct(GitHubContext $context, Configuration $configuration)
     {
         $this->client = new BadgesClient($context::getGitHubToken());
         $this->context = $context;
@@ -47,10 +47,10 @@ class UpdateBadgesAction implements Action
                     'style' => $results->getStyle(),
                 ],
                 'requirements' => [
-                    'min-code' => 80,
-                    'min-architecture' => 80,
-                    'min-complexity' => 80,
-                    'min-style' => 80,
+                    'min-code' => $this->configuration->getMinQuality(),
+                    'min-architecture' => $this->configuration->getMinArchitecture(),
+                    'min-complexity' => $this->configuration->getMinComplexity(),
+                    'min-style' => $this->configuration->getMinStyle(),
                 ],
             ]
         );
