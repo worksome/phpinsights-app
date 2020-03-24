c64289d4c5194f4861f04e32106d51492361b190
Oliver Nybroe <olivernybroe@gmail.com>
2020-03-24 10:19:31 +0100

    feat(docker): Add support for setting memory limit

--- action/entrypoint.sh
+++ action/entrypoint.sh
@@ -1,3 +1,7 @@
 #!/bin/sh -l
 
-php /action/phpinsights-app.phar $GITHUB_WORKSPACE $*
+if [ -z $INPUT_MEMORY_LIMIT ]; then
+  INPUT_MEMORY_LIMIT="128M"
+fi
+
+php -d memory_limit=$INPUT_MEMORY_LIMIT /action/phpinsights-app.phar $GITHUB_WORKSPACE $*
